f7a9874828282d8da3f33d6a14e24b196a9365a8
goven-space <439023672@qq.com>
2021-04-25 07:50:12 +0800

    扫雷游戏

--- index.js
+++ index.js
@@ -3,20 +3,33 @@ class Mine {
         this.trNums = trNums//行数
         this.tdNums = tdNums//列数
         this.mineNums = mineNums//地雷数
-        this.squareArr = [] //总格子二维数组
-        this.eleArr = []
+        this.squareArr = [] //二维数组的格式放置每格的属性
+        //属性格式
+        // {
+        //     type: "number"/"mine"
+        //     x:
+        //     y:
+        //     value 
+        // }
+        this.eleArr = []    //二维数组的格式放置每格的dom元素
+        this.overCount = 0
 
         this.total = trNums * tdNums//格子总数
-        this.parent = document.querySelector('.wrapper')
+        this.parent = document.querySelector('.gameBox')
+        this.mn = document.querySelector('.mine-nums')
     }
     //创建元素
     createDom() {
         var self = this
+        self.parent.innerHTML = ''
         var table = document.createElement('table')
+        //初始化地雷数
+        this.mn.innerHTML = this.mineNums
         //取消右键打开菜单
         table.oncontextmenu = function () {
             return false
         }
+        //开始创建表格
         for (let i = 0; i < this.trNums; i++) {
             var tr = document.createElement('tr')
             this.eleArr[i] = []
@@ -33,8 +46,9 @@ class Mine {
             }
             table.appendChild(tr)
         }
-        var wrapper = document.querySelector('.wrapper')
-        this.parent.appendChild(table)
+        
+        self.parent.innerHTML = ''//避免重复创建表格
+        self.parent.appendChild(table)
     }
     //随机生成地雷位置
     randomNum() {
@@ -47,10 +61,13 @@ class Mine {
         })
         return square.slice(0, this.mineNums)
     }
+    //初始化
     init() {
         this.createDom()
+        this.overCount = 0
         var mineList = this.randomNum()
         var n = 0
+        //以二维数组格式添加表格属性
         for (let i = 0; i < this.trNums; i++) {
             this.squareArr[i] = []
             for (let j = 0; j < this.tdNums; j++) {
@@ -73,7 +90,7 @@ class Mine {
         this.updateNum()
 
     }
-    //更新每个格子的数字
+    //更新表格中代表周围地雷个数的数字
     updateNum() {
         for (let i = 0; i < this.trNums; i++) {
             for (let j = 0; j < this.tdNums; j++) {
@@ -88,8 +105,8 @@ class Mine {
                 }
             }
         }
-        console.log(this.squareArr)
     }
+    //获取点击表格周围9宫格内的表格的位置以行列表示返回
     around(x, y) {
         let aroundArr = []
 
@@ -105,15 +122,24 @@ class Mine {
         return aroundArr
     }
 
+    //游戏运行主方法
     play(env, obj) {
         var cl = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"]
         var self = this
-        //显示格子
+        //显示格子方法
+        //1、点击格子为地雷是，游戏失败
+        //2、点击格子不是地雷时，分两种情况1.该格子9宫格范围内地雷为0时，将与该格子
+        //相连的格子显示，知道其格子9宫格内地雷数不为0为止
+        //2.若该格子9宫格内地雷数不为0，则显示该地雷数
         function show(state, ele) {
             if (!state.check) {
                 state.check = true
                 if (state.value === 0) {
+                    self.overCount++
                     ele.classList.add('zero')
+                    if(self.overCount == self.total-self.mineNums){
+                        self.over(true)
+                    }
                     var around = self.around(state.x, state.y)
                     for (var value of around) {
                         if (!self.eleArr[value[0]][value[1]].classList.contains('flag')) {
@@ -122,8 +148,12 @@ class Mine {
 
                     }
                 } else {
+                    self.overCount++
                     ele.classList.add(cl[state.value])
                     ele.innerHTML = state.value
+                    if(self.overCount == self.total-self.mineNums){
+                        self.over(true)
+                    }
                 }
 
 
@@ -135,38 +165,94 @@ class Mine {
             var ele = this.eleArr[state.y][state.x]
             if (state.type == "mine") {
                 ele.classList.add('bomb')
-                for (let i = 0; i < this.trNums; i++) {
-                    for (let j = 0; j < this.tdNums; j++) {
-                        var square = this.squareArr[i][j]
-
-                        if (square.type == "mine") {
-                            this.eleArr[i][j].classList.add('mine')
-                        }
-                        this.eleArr[i][j].onmousedown = null
-                    }
-                }
-                setTimeout(() => {
-                    alert("Game Over")
-                },0)
+                this.over()             
             }
             if (!obj.classList.contains('flag')) {
                 if (state.type == "number") {
                     show(state, ele)
                 }
             }
-
+            //点击鼠标右键
         } else if (env.which == 3) {
             if (!state.check) {
                 if (obj.classList.contains('flag')) {
+                    this.mineNums++
+                    this.mn.innerHTML = this.mineNums
                     obj.classList.remove('flag')
                 } else {
+                    this.mineNums--
+                    this.mn.innerHTML = this.mineNums
                     obj.classList.add('flag')
+                }
+                if(this.mineNums ==0){
+                    this.checkResult()
                 }
             }
 
 
         }
     }
+    //检测所有旗子标记的地方是否为地雷
+    checkResult(){
+        for (let i = 0; i < this.trNums; i++) {
+            for (let j = 0; j < this.tdNums; j++) {
+                var square = this.squareArr[i][j]
+                if (square.type == "mine") {
+                    if(!this.eleArr[i][j].classList.contains('flag')){
+                        this.over(false)
+                        return
+                    }
+                    
+                }
+                
+            }
+        }
+        this.over(true)
+    }
+
+    //游戏结束 分为游戏胜利 和游戏失败两种结果
+    over(result){
+        for (let i = 0; i < this.trNums; i++) {
+            for (let j = 0; j < this.tdNums; j++) {
+                var square = this.squareArr[i][j]
+
+                if (square.type == "mine") {
+                    this.eleArr[i][j].classList.add('mine')
+                }
+                this.eleArr[i][j].onmousedown = null
+            }
+        }
+        setTimeout(() => {
+            if(result){
+                alert("恭喜你过关了")
+            }else{
+                alert("很遗憾你失败了")
+            }
+        },100)
+        
+        
+    }
 }
-var mine = new Mine(15, 15, 22)
-mine.init()
+//顶排按键功能
+var mine 
+var fir = 0
+var btn = document.querySelectorAll('.level-btn')
+var arr = [[9,9,10],[16,16,40],[25,25,75]]
+for(let i =0 ;i<btn.length;i++){
+    if(i<3){
+        btn[i].onclick = () => {
+            mine = new Mine(...arr[i])
+            mine.init()
+            btn[fir].classList.remove('active')
+            btn[i].classList.add('active')
+            fir = i
+        }
+    }else{
+        btn[i].onclick = () => {
+            mine.init()
+        }
+    }
+}
+btn[0].onclick()
+
+
